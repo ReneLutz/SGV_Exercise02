8171ca2ac5dcaf9d8eb5aed17e229da3459bb624
Rene <rene.lutz@stud.tu-darmstadt.de>
2020-06-01 19:54:59 +0200

    Added highscore board

---
+++ Lutz_Rene_Uebung_05/Assets/Scripts/HighscoreEntryUI.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class HighscoreEntryUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _position;
+    [SerializeField] private TextMeshProUGUI _score;
+
+    public virtual HighscoreEntryUI Init(string position, string score)
+    {
+        _position.text = position;
+        _score.text = score;
+
+        return this;
+    }
+}
---
+++ Lutz_Rene_Uebung_05/Assets/Scripts/HighscoreTable.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class HighscoreTable : MonoBehaviour
+{
+    [SerializeField] private HighscoreEntryUI _entryPrefab;
+    [SerializeField] private TextMeshProUGUI _scoreUI;
+
+    private List<HighscoreEntryUI> _tableEntries = new List<HighscoreEntryUI>();
+
+    private readonly string JSON_KEY = "Highscores{0}";
+   
+    private int[] _scores = new int[5];
+
+    private Transform _transform;
+
+    private void Awake()
+    {
+        _transform = transform;
+
+        LoadAndSortHighscoreTable();
+        
+        int currentScore = Score.GetScore();
+        Score.Reset();
+
+        _scoreUI.text = string.Format("Score: {0}", currentScore);
+
+        // Write new Highscore and save list
+        if (currentScore > _scores[0])
+        {
+            _scores[0] = currentScore;
+
+            Array.Sort(_scores);
+
+            SaveHighscoreTable();
+        }
+
+        for (int i = _scores.Length - 1; i >= 0; i--)
+        {
+            AddEntry(_scores.Length - i, _scores[i]);
+        }
+    }
+
+    private void LoadAndSortHighscoreTable()
+    {
+        for (int i = 0; i < _scores.Length; i++)
+        {
+            _scores[i] = PlayerPrefs.GetInt(String.Format(JSON_KEY, i), 0);
+        }
+        Array.Sort(_scores);
+    }
+
+    private void SaveHighscoreTable()
+    {
+        for (int i = 0; i < _scores.Length; i++)
+        {
+            PlayerPrefs.SetInt(String.Format(JSON_KEY, i), _scores[i]);
+        }
+        PlayerPrefs.Save();
+    }
+
+    private void AddEntry(int position, int score)
+    {
+        HighscoreEntryUI entry = Instantiate(_entryPrefab, _transform);
+        entry.Init(string.Format("{0}.", position.ToString()), score.ToString());
+
+        entry.transform.SetParent(_transform);
+        
+        _tableEntries.Add(entry);
+    }
+
+}
